9c715b82c1c384d1eb291d3d0992ffe4d75c00e5
Błażej Piaskowski <haradd@users.noreply.github.com>
2020-06-26 23:23:29 +0200

    add ngrok proxy to whitelist

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "7bddb7d9bfbb.ngrok.io"
+
 end
