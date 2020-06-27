6ba38ad7d49e11c62e464d2fd53537ca1dfd8a2b
Błażej Piaskowski <haradd@users.noreply.github.com>
2020-06-27 18:47:11 +0200

    add helper method to render jsons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ::ActionController::API
   def authorize_user
     warden.authenticate!
   end
+
+  def render_json(data, status = :ok)
+    render json: { data: data }, status: status
+  end
+
+  def render_errors(hash)
+    render json: { errors: hash }, status: :unprocessable_entity
+  end
 end
